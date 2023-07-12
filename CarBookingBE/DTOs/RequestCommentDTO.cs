1c9c58ab897462234f9f50af7b6082993cbe8775
Nam Ho Van <vannam272008@gmail.com>
2023-07-12 10:46:02 +0700

    remove RequestCommentDTO

--- CarBookingBE/DTOs/RequestCommentDTO.cs
+++ CarBookingBE/DTOs/RequestCommentDTO.cs
@@ -10,14 +10,10 @@ namespace CarBookingBE.Controllers
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
-        public Guid? UserId { get; set; }
-        [ForeignKey("UserId")]
         public AccountDTO Account { get; set; }
         public string Content { get; set; }
         public DateTime Created { get; set; }
-        public Guid? RequestId { get; set; }
-        [ForeignKey("RequestId")]
-        public Request Request { get; set; }
+        public RequestDTO Request { get; set; }
         public bool IsDeleted { get; set; }
     }
 }

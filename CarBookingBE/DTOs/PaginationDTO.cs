d0ce34438847bb280686a887d9ce401064818fa2
Nam Ho Van <vannam272008@gmail.com>
2023-07-20 13:51:28 +0700

    add PaginationDTO

--- CarBookingBE/DTOs/PaginationDTO.cs
+++ CarBookingBE/DTOs/PaginationDTO.cs
@@ -7,9 +7,11 @@ namespace CarBookingBE.DTOs
 {
     public class PaginationDTO<T>
     {
+
         public int PerPage { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPage { get; set; }
+
         public List<T> ListData { get; set; }
     }
 }
